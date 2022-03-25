c9d8cdcea86a07f89613b00efa404b2dba356a65
bmulamba <badibangamulamba@gmail.com>
2022-03-25 05:32:43 +0200

    first deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import UserCard from './Components/UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users : []
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(resp => resp.json())
+    .then(person => this.setState({users : person}));
+  }
+  render() {
+    // console.log(this.state.users);
+    return (
+      <div className='container'>
+        {
+          this.state.users.map(user => {
+            return(
+              <div key = {user.id} >
+                <UserCard 
+                  userId = {user.id}
+                  name = {user.name}
+                  email = {user.email}
+                />
+               </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/UserCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./../App.css"
+
+export default function UserCard(props) {
+    // console.log(props.userId);
+  return (
+  
+    <div className='container-card'>
+        <div className='card-image'>
+            <img src={`https://robohash.org/${props.userId}/?set_set3/size=140x140`} alt={props.id} />
+        </div>
+        <div className='card-body'>
+            <span>{props.name}</span><br/>
+            <span>{props.email}</span>
+        </div>
+        
+    </div>
+
+  )
+}
