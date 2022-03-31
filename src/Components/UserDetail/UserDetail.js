ef382d3f14e115e75dea72cf7d5af13dd2124b7a
bmulamba <badibangamulamba@gmail.com>
2022-03-31 11:36:14 +0200

    updatepr

--- src/Components/UserDetail/UserDetail.js
+++ src/Components/UserDetail/UserDetail.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export const UserDetail = (props) => {
-  var userId = props.match.params.id
+  // var userId = props.match.params.id
   return (
     <div>
     <div>UserDetail</div>
-    <div> {userId} </div>
+    {/* <div> {userId} </div> */}
     </div>
   )
 }
